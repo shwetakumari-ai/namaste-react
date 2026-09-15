820f87f0ab4aa86e3ebc4ce434d0b2392763d56f
Shwetakumari-ai <swati980123rs@gmail.com>
2026-09-16 01:01:08 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+//const heading = React.createElement("h1", { id: "heading" }, "Hello world from namaste react!");
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag")
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag"),
+    ]),
+]);
+//console.log(heading); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
